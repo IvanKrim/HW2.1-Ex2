e2715d68e363dfc5a2a0712262ece6dd358c2508
JohnKrim <reamonn96@gmail.com>
2021-03-23 12:33:19 +0300

    Initial Commit

---
+++ HW2.1 Ex2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1 Ex2
+//
+//  Created by Kalabishka Ivan on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
